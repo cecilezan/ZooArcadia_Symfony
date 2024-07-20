938bf6b33f3ee6ffec2fb491268fab0693f928af
Zanco <cecilezanconato@free.fr>
2024-07-20 17:54:07 +0200

    CRUD registration

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
+use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -10,24 +12,76 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
+use OpenApi\Attributes as OA;
+use OpenApi\Attributes\RequestBody;
+use OpenApi\Attributes\Property;
+use OpenApi\Attributes\MediaType;
+use OpenApi\Attributes\Schema;
+use OpenApi\Attributes\Response as OAResponse;
+
+
 
 #[Route('/api', name: 'app_api_')]
 class SecurityController extends AbstractController
 {
     public function __construct(
-        private SerializerInterface $serializer,
         private EntityManagerInterface $manager,
-    )
+        private UserRepository $repository,
+        private SerializerInterface $serializer,
+        private UrlGeneratorInterface $urlGenerator)
     {
     }
+
     #[Route('/registration', name: 'registration', methods: ['POST'])]
+
+    #[OA\Post(
+        path:"/api/registration",
+        summary: "Inscription d'un nouvel utilisateur",
+        requestBody: new RequestBody(
+            required: true,
+            description: "Données de l'utilisateur à inscrire",
+            content: [new Mediatype(mediaType:"application/json",
+                schema: new Schema(type:"object", properties:[new Property(
+                    property: "email",
+                    type: "string",
+                    example: "john@doe.com"
+                ),
+                    new Property(
+                        property: "password",
+                        type: "string",
+                        example: "Mot de passe"
+                    )]))]
+        )/*,
+        responses: new OAResponse(
+            response: 201,
+            description: "Utilisateur inscrit avec succès",
+            content: [new MediaType(mediaType:"application/json",
+            schema: new Schema(type:"object", properties:[new Property(
+                property: "user",
+                type: "string",
+                example: "john@doe.com"
+                ),
+                    new Property(
+                        property: "apiToken",
+                        type: "string",
+                        example :"5ec84856df78ba8cdcecea5fe1e749edb83a6d4a292b89b62fad2720cdb89a6f"
+                    ),
+                    new Property(
+                        property: "roles",
+                        type: "array",
+                        example: ["ROLE_USER"]
+                    )]))]
+        )*/
+    )]
     public function register(Request $request, UserPasswordHasherInterface $passwordHasher): JsonResponse
     {
         $user = $this->serializer->deserialize($request->getContent(),User::class, 'json');
         $user->setPassword($passwordHasher->hashPassword($user,$user->getPassword()));
-        $user->setCreatedAt(new \DateTimeImmutable());
+        $user->setCreatedAt(new DateTimeImmutable());
 
         $this->manager->persist($user);
         $this->manager->flush();
@@ -52,6 +106,52 @@ class SecurityController extends AbstractController
             'apiToken'=>$user->getApiToken(),
             'roles'=>$user->getRoles()
         ]);
+    }
+
+    #[Route('/registration/{id}', name: 'show', methods: 'GET')]
+    public function show(int $id): JsonResponse
+    {
+        $user = $this->repository->findOneBy(['id' => $id]);
+        if($user) {
+            $responseData = $this->serializer->serialize($user, 'json');
+            return new JsonResponse($responseData, Response::HTTP_OK, [], true);
+
+        }
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+    }
+
+    #[Route('/registration/{id}', name: 'edit', methods: 'PUT')]
+    public function edit(int $id, Request $request): JsonResponse
+    {
+        $user = $this->repository->findOneBy(['id' => $id]);
+
+        if ($user){
+            $user = $this->serializer->deserialize($request->getContent(),
+                User::class,
+                'json',
+                [AbstractNormalizer::OBJECT_TO_POPULATE => $user]);
+
+            $this->manager->flush();
+            return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+        }
+
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND, [], true);
+    }
+    #[Route('/registration/{id}', name: 'delete', methods: 'DELETE')]
+    public function delete(int $id): JsonResponse
+    {
+        $user = $this->repository->findOneBy(['id' => $id]);
+
+        if($user){
+            $responseData = $this->serializer->serialize($user, 'json');
+            if(!in_array("ROLE_ADMIN", $user->getRoles())){
+                $this->manager->remove($user);
+                $this->manager->flush();
+            }
+            return new JsonResponse(null, Response::HTTP_FORBIDDEN);
+        }
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+
     }
 }
 
